10e0401bad548188a79053611bf2313c9d1d5aa8
LAP <2300820209@qq.com>
2019-01-28 19:38:21 +0800

    图片大小优化

--- all_stack_front/vue.config.js
+++ all_stack_front/vue.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const productionGzipExtensions = ['js', 'css'];
 const debug = process.env.NODE_ENV !== 'production';
 module.exports = {
     baseUrl:'/',//根域上下文目录
@@ -7,17 +9,39 @@ module.exports = {
     lintOnSave:false,//是否开启eslint保存检测，有效值：true|false|'error'
     runtimeCompiler:true,//运行时版本是否需要编译
     // transpileDepencies:[],
-    productionSourceMap:true,
+    productionSourceMap:false,
     configureWebpack:(config)=>{
         if(debug){//开发环境配置
             config.devtool = 'cheap-module-eval-source-map';
+            // config.plugins.push(new CompressionWebpackPlugin({
+            //     algorithm: 'gzip',
+            //     test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+            //     threshold: 10240,
+            //     minRatio: 0.8
+            //   }));
         }else{//生产环境配置
-
+            // config.plugins.push(
+            //     new CompressionWebpackPlugin({
+            //         asset: '[path].gz[query]',
+            //         algorithm: 'gzip',
+            //         test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+            //         threshold: 10240,
+            //         minRatio: 0.8
+            //     })
+            // )
         }
     },
-    // chainWebpack:(config)=>{
-
-    // }
+    chainWebpack:(config)=>{
+        config.module
+            .rule('image-webpack-loader')
+            .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
+            .use('image-webpack-loader')
+            // .loader('url-loader')
+            .loader('image-webpack-loader')
+            .options({
+                    limit: 10000
+            })            
+    },
     parallel:require('os').cpus().length > 1,//构建时开启多进程处理babel编译
     pluginOptions:{
 
